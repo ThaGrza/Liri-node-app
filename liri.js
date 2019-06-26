bf5e41b2b48e8dc1a0e0aaecca10728e45fc8ed8
Andrew Griswold <andrewgriswold7@gmail.com>
2019-06-26 00:01:21 -0500

    Fixed spotify to currently working

--- liri.js
+++ liri.js
@@ -9,6 +9,7 @@ var spotify = new Spotify ({
     secret: "feb2d844093748839afbbe6b2fd9ff16"
 });
 
+// var userChoice = "";
 var userChoice = process.argv.slice(3);
 // var queryUrl = "http://www.omdbapi.com/?t=" + userChoice + "&y=&plot=short&apikey=trilogy";
 
@@ -35,11 +36,31 @@ function getMovie (queryUrl){
             console.log("Plot: " + response.data.Plot);
         })
 }
-// LOGIC FOR SPOTIFY----------------------------------------------------------------
+// LOGIC FOR SPOTIFY----------------UNFINISHED----------------------------
 if (process.argv[2] === "spotify-this-song" && userChoice !==''){
-    spotify.search({ type: 'track', query: userChoice }, function(err, response) {
-        
-    console.log(response.tracks.items.album);
+    spotify.search({ type: 'track', query: userChoice, limit: 1}, function(err, response) {
+        var spotifyArr = response.tracks.items;
+        for (var i = 0; i < spotifyArr.length; i++){
+            console.log("Artist: ", response.tracks.items[i].album.artists[0].name);
+            console.log("Song: ", response.tracks.items[i].name);
+            console.log("Album: ", response.tracks.items[i].album.name);
+            console.log("Spotify Link: ", response.tracks.items[i].external_urls.spotify);
+        }
+    // console.log(response.tracks);
     });
 };
 
+// LOGIC FOR CONCERT EVENT INFO------------------------------------------------------------
+if (process.argv[2] === "concert-this" && userChoice !==''){
+    var queryUrl = "https://rest.bandsintown.com/artists/" + userChoice + "/events?app_id=codingbootcamp";
+    getConcert();
+
+}
+
+function getConcert (queryUrl){
+    axios.get(queryUrl).then(
+        function(response) {
+            console.log(response);
+            
+        })
+}
