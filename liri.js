c336a86fcc2e123a465b7e5fbdc4c4745d9aa45f
Andrew Griswold <andrewgriswold7@gmail.com>
2019-06-25 23:23:07 -0500

    Added search movie functionality and not finished spotify song search

--- liri.js
+++ liri.js
@@ -3,4 +3,43 @@ require("dotenv").config();
 var axios = require("axios");
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify ({
+    id: "6dd0566a11204578abd841fd17a3f1fc",
+    secret: "feb2d844093748839afbbe6b2fd9ff16"
+});
+
+var userChoice = process.argv.slice(3);
+// var queryUrl = "http://www.omdbapi.com/?t=" + userChoice + "&y=&plot=short&apikey=trilogy";
+
+console.log(userChoice);
+
+
+// LOGIC FOR MOVIES -----------------------------------------------------------
+if (process.argv[2] === "movie-this" && userChoice !==''){
+    var queryUrl = "http://www.omdbapi.com/?t=" + userChoice + "&y=&plot=short&apikey=trilogy";
+    getMovie(queryUrl);
+} else if (process.argv[2] === "movie-this" && userChoice ==''){
+    var queryUrl = "http://www.omdbapi.com/?t='Mr.Nobody'&y=&plot=short&apikey=trilogy";
+};
+function getMovie (queryUrl){
+    axios.get(queryUrl).then(
+        function(response) {
+            console.log("Title: " + response.data.Title);
+            console.log("Actors: " + response.data.Actors);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Metascore);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+        })
+}
+// LOGIC FOR SPOTIFY----------------------------------------------------------------
+if (process.argv[2] === "spotify-this-song" && userChoice !==''){
+    spotify.search({ type: 'track', query: userChoice }, function(err, response) {
+        
+    console.log(response.tracks.items.album);
+    });
+};
+
